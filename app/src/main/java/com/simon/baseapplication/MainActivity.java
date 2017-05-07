0dc26f5bd6b731889692a2d29f7837757996cb8b
simonfong <fongzm_simon@163.com>
2017-05-07 09:43:28 +0800

    first commit

---
+++ app/src/main/java/com/simon/baseapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.simon.baseapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "这是一个吐司", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(MainActivity.this, "添加", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(MainActivity.this, "删除", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+
+        }
+        return true;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+}
---
+++ app/src/main/java/com/simon/baseapplication/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.simon.baseapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by simon on 2017/5/5.
+ */
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_splash);
+    }
+}
